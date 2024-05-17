bc1372be88218760d3485d7c65bc6eeb7d258b3c
chocolee1020@gmail.com <eson102000>
2024-05-17 22:41:28 +0800

    [init]加上顯示座標在地圖上

--- app/src/main/java/com/atakmap/android/plugintemplate/PluginTemplateDropDownReceiver.java
+++ app/src/main/java/com/atakmap/android/plugintemplate/PluginTemplateDropDownReceiver.java
@@ -4,16 +4,22 @@ package com.atakmap.android.plugintemplate;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 
 import com.atak.plugins.impl.PluginLayoutInflater;
+import com.atakmap.android.ipc.AtakBroadcast;
+import com.atakmap.android.maps.MapGroup;
 import com.atakmap.android.maps.MapView;
+import com.atakmap.android.maps.Marker;
 import com.atakmap.android.plugintemplate.bluetooth.RcManager;
 import com.atakmap.android.plugintemplate.plugin.R;
 import com.atakmap.android.dropdown.DropDown.OnStateListener;
 import com.atakmap.android.dropdown.DropDownReceiver;
 
 import com.atakmap.coremap.log.Log;
-import com.jeremyliao.liveeventbus.LiveEventBus;
+import com.atakmap.coremap.maps.coords.GeoPoint;
+
+import java.util.UUID;
 
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.disposables.Disposable;
@@ -42,8 +48,47 @@ public class PluginTemplateDropDownReceiver extends DropDownReceiver implements 
             rcManager.onStart(context);
         }).subscribeOn(Schedulers.io()).subscribe();
 
-    }
+        final Button wheel = templateView
+                .findViewById(R.id.mark_button);
 
+        wheel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createUnit( 24.1234, 121.567);
+
+            }
+        });
+
+    }
+    public void createUnit(double latitude, double longitude) {
+
+
+        // 創建包含指定經緯度的 GeoPoint 對象
+        GeoPoint point = new GeoPoint(latitude, longitude);
+
+        Marker m = new Marker(point, UUID.randomUUID().toString());
+
+        m.setType("a-f-G-U-C-I");
+
+        m.setMetaBoolean("editable", true);
+        m.setMetaBoolean("removable", true);
+
+        m.setTitle("latitude=24.1234, longitude=121.567");
+
+        MapGroup _mapGroup = getMapView().getRootGroup()
+                .findMapGroup("Cursor on Target")
+                .findMapGroup("Friendly");
+        _mapGroup.addItem(m);
+
+        m.persist(getMapView().getMapEventDispatcher(), null,
+                this.getClass());
+
+        Intent new_cot_intent = new Intent();
+        new_cot_intent.setAction("com.atakmap.android.maps.COT_PLACED");
+        new_cot_intent.putExtra("uid", m.getUID());
+        AtakBroadcast.getInstance().sendBroadcast(
+                new_cot_intent);
+    }
     /**************************** PUBLIC METHODS *****************************/
 
     public void disposeImpl() {
--- app/src/main/java/com/atakmap/android/plugintemplate/bluetooth/RemoteControlBleNotifyEvent.java
+++ app/src/main/java/com/atakmap/android/plugintemplate/bluetooth/RemoteControlBleNotifyEvent.java
@@ -6,7 +6,7 @@ import android.util.Log;
 import com.atakmap.android.plugintemplate.BluetoothConfig;
 import com.clj.fastble.callback.BleNotifyCallback;
 import com.clj.fastble.exception.BleException;
-import com.jeremyliao.liveeventbus.LiveEventBus;
+//import com.jeremyliao.liveeventbus.LiveEventBus;
 
 public class RemoteControlBleNotifyEvent extends BleNotifyCallback {
 
@@ -23,6 +23,6 @@ public class RemoteControlBleNotifyEvent extends BleNotifyCallback {
     @Override
     public void onCharacteristicChanged(byte[] data) {
         String value = new String(data).trim();
-        LiveEventBus.<String>get(BluetoothConfig.RC_DATA_TOPIC).post(value);
+//        LiveEventBus.<String>get(BluetoothConfig.RC_DATA_TOPIC).post(value);
     }
 }
